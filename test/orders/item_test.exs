c16ae3bcf2facfc7845c1b831ad09b1caf30fef1
Ayrton Souza <ayrtonsouza@icloud.com>
2021-05-06 17:16:47 -0300

    :white_check_mark: : adding orders unit test

--- test/orders/item_test.exs
+++ test/orders/item_test.exs
@@ -9,25 +9,25 @@ defmodule Exlivery.Orders.ItemTest do
     test "when all params are valid, return an item" do
       response = Item.build("Pizza de Peperoni", :pizza, "35.5", 1)
       expected_response = {:ok, build(:item)}
-      assert expected_response == response
+      assert response == expected_response
     end
 
     test "when the is an invalid category, returns an error" do
       response = Item.build("Pizza de Peperoni", :invalid, "35.5", 1)
       expected_response = {:error, "Invalid parameters"}
-      assert expected_response == response
+      assert response == expected_response
     end
 
     test "when there is an invalid price, returns an error" do
       response = Item.build("Pizza de Peperoni", :pizza, "Invalid price", 1)
       expected_response = {:error, "Invalid price"}
-      assert expected_response == response
+      assert response == expected_response
     end
 
     test "when there is an invalid quantity, returns an error" do
       response = Item.build("Pizza de Peperoni", :pizza, "35.5", 0)
       expected_response = {:error, "Invalid parameters"}
-      assert expected_response == response
+      assert response == expected_response
     end
   end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Orders.Item
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
 
   def item_factory do
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       cpf: "12345678900",
       age: 25,
       address: "Avenida Paulista, 500"
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Avenida Paulista, 500",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unit_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50"),
+      user_cpf: "12345678900"
     }
   end
 end

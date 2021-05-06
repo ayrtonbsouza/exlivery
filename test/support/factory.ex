5b21268441db915be125c45d3db88343aff83d0f
Ayrton Souza <ayrtonsouza@icloud.com>
2021-05-06 16:55:52 -0300

    :white_check_mark: : adding items unit tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,8 +1,17 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Users.User
   alias Exlivery.Orders.Item
+  alias Exlivery.Users.User
+
+  def item_factory do
+    %Item{
+      description: "Pizza de Peperoni",
+      category: :pizza,
+      unit_price: Decimal.new("35.5"),
+      quantity: 1
+    }
+  end
 
   def user_factory do
     %User{
@@ -11,15 +20,6 @@ defmodule Exlivery.Factory do
       cpf: "12345678900",
       age: 25,
       address: "Avenida Paulista, 500"
-    }
-  end
-
-  def item_factory do
-    %Item{
-      description: "Pizza de Peperoni",
-      category: :pizza,
-      unit_price: Decimal.new("35.5"),
-      quantity: 1
     }
   end
 end
